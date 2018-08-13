f37402c14f67b4aee8a535f8b3fa267d221e70df
Matej Korman <matejkorman@matejs-mbp>
2018-08-13 12:29:04 +0200

    Initial Commit

---
+++ FactoryWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FactoryWeather
+//
+//  Created by Matej Korman on 13/08/2018.
+//  Copyright © 2018 Matej Korman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
